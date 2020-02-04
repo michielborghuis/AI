dfa1bbc3d8e49f60f7db541907ce4f87315fc061
michielborghuis <59872213+michielborghuis@users.noreply.github.com>
2020-02-04 14:36:37 +0100

    Monoliet.

--- pyramide.py
+++ pyramide.py
@@ -1,6 +1,20 @@
-lenght = int(input('Hoe groot? '))
+def piramide_for():
+    lenght = int(input('Hoe groot? '))
 
-for i in range(lenght+1):
-    print('*' * i)
-for i in range(lenght):
-    print('*' * (lenght-i))
+    for i in range(lenght+1):
+        print('*' * i)
+    for i in range(lenght):
+        print('*' * (lenght-i))
+
+def piramide_while():
+    lenght = int(input('Hoe groot? '))
+    count = 1
+    while count < lenght:
+        print('*' * count)
+        count += 1
+    while count > 0:
+        print('*' * count)
+        count -= 1
+
+piramide_for()
+piramide_while()
