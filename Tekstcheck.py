8959032f0f7e0311f36127d1fb28998ab0667998
michielborghuis <59872213+michielborghuis@users.noreply.github.com>
2020-02-06 12:05:08 +0100

    Monoliet.

--- Tekstcheck.py
+++ Tekstcheck.py
@@ -1,7 +1,11 @@
 def tekstcheck():
     str1 = input('Geef een string: ')
     str2 = input('Geef een string: ')
-    for i in range(0, len(a))
-
+    if str1 < str2:
+        str1, str2 = str2, str1
+    for i in range(0, len(str1)):
+        if str1[i] is not str2[i]:
+            print('Het eerste verschil zit op index: ' + str(i))
+            break
 
 tekstcheck()
